dea5a0756d1c5d43ccd14fb7b2bc3895479de8da
Fouad Karam <fouadk13@gmail.com>
2022-01-14 17:56:55 -0500

    Started JS implementation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+const questionBtn = document.getElementById("question-btn")
+const answerBtn = document.getElementById("answer-btn")
+
+const table1 = document.getElementById("table-el1") 
+
+table1.addEventListener("click", function() {
+    console.log("Table 1 Clicked")
+})
+
+questionBtn.addEventListener("click", function() {
+    console.log("New Question Clicked");
+})
+
+answerBtn.addEventListener("click", function() {
+    console.log("New Answer Clicked");
+})
