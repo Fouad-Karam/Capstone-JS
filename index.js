249d5869aab3255b77f350b034945acff5285514
Fouad Karam <fouadk13@gmail.com>
2022-01-15 00:49:13 -0500

    Micro updates

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-
+/*Const variables*/
 const questionBtn = document.getElementById("question-btn")
 const answerBtn = document.getElementById("answer-btn")
 
-const table1 = document.getElementById("table-el1") 
+const table1 = document.getElementById("table-el1")
+const table2 = document.getElementById("table-el2") 
+const table3 = document.getElementById("table-el3") 
+const table4 = document.getElementById("table-el4") 
+const table5 = document.getElementById("table-el5") 
+const table6 = document.getElementById("table-el6") 
+const table7 = document.getElementById("table-el7") 
+const table8 = document.getElementById("table-el8") 
+const table9 = document.getElementById("table-el9") 
+const table10 = document.getElementById("table-el10") 
+const table11 = document.getElementById("table-el11") 
+const table12 = document.getElementById("table-el12") 
 
+
+/*Multiplication table functions*/
 table1.addEventListener("click", function() {
     console.log("Table 1 Clicked")
 })
+table2.addEventListener("click", function() {
+    console.log("Table 2 Clicked")
+})
+table3.addEventListener("click", function() {
+    console.log("Table 3 Clicked")
+})
+table4.addEventListener("click", function() {
+    console.log("Table 4 Clicked")
+})
+table5.addEventListener("click", function() {
+    console.log("Table 5 Clicked")
+})
+table6.addEventListener("click", function() {
+    console.log("Table 6 Clicked")
+})
+table7.addEventListener("click", function() {
+    console.log("Table 7 Clicked")
+})
+table8.addEventListener("click", function() {
+    console.log("Table 8 Clicked")
+})
+table9.addEventListener("click", function() {
+    console.log("Table 9 Clicked")
+})
+table10.addEventListener("click", function() {
+    console.log("Table 10 Clicked")
+})
+table11.addEventListener("click", function() {
+    console.log("Table 11 Clicked")
+})
+table12.addEventListener("click", function() {
+    console.log("Table 12 Clicked")
+})
 
+
+/*Question & Answer functions*/
 questionBtn.addEventListener("click", function() {
     console.log("New Question Clicked");
 })
